403ee5db0530671034dba966d62afa8e215d0c37
hi <hoang>
2021-06-21 10:44:31 +0700

    db PROD

--- config/auth.js
+++ config/auth.js
@@ -1,20 +1,19 @@
-require('dotenv').config();
 module.exports = {
     user          : "admin",
-  
+
     // Get the password from the environment variable
     // NODE_ORACLEDB_PASSWORD.  The password could also be a hard coded
     // string (not recommended), or it could be prompted for.
     // Alternatively use External Authentication so that no password is
     // needed.
-    // password      : process.env.PASSWORD,
-    password : "admin200%scrp",
+    password      : "admin100%scrp",
+
     // For information on connection strings see:
     // https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings
     // connectString : "localhost/infobox",
-    // connectString : "192.168.100.19/xe",
-    connectString : "10.84.243.126:1522/VNDEV",
+    connectString : "10.84.23.122:1577/VNDPG",
+
     // Setting externalAuth is optional.  It defaults to false.  See:
     // https://oracle.github.io/node-oracledb/doc/api.html#extauth
     // externalAuth  : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
-  };
+};
